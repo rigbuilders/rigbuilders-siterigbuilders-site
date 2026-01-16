b6b16524198ac68eb53358e701afc24f87fe237a
rigbuilders <ginniomen22@gmail.com>
2026-01-16 16:35:05 +0530

    seo view

--- components/SchemaJsonld.tsx
+++ components/SchemaJsonld.tsx
@@ -1,26 +1,44 @@
 export default function SchemaJsonLd() {
-  const jsonLd = {
+  const organizationSchema = {
     "@context": "https://schema.org",
     "@type": "Organization",
     "name": "Rig Builders",
     "url": "https://www.rigbuilders.in",
-    "logo": "https://www.rigbuilders.in/icons/navbar/logo.png", // Must be absolute URL
+    "logo": "https://www.rigbuilders.in/icons/navbar/logo.png",
     "sameAs": [
-      "https://instagram.com/rigbuilders", // Add your socials
+      "https://instagram.com/rigbuilders",
       "https://twitter.com/rigbuilders"
     ],
     "contactPoint": {
       "@type": "ContactPoint",
-      "telephone": "+91-XXXXXXXXXX", // Add your support number
+      "telephone": "+91-XXXXXXXXXX",
       "contactType": "customer service",
       "areaServed": "IN"
     }
   };
 
+  const websiteSchema = {
+    "@context": "https://schema.org",
+    "@type": "WebSite",
+    "name": "Rig Builders",
+    "url": "https://www.rigbuilders.in",
+    "potentialAction": {
+      "@type": "SearchAction",
+      "target": "https://www.rigbuilders.in/products?search={search_term_string}",
+      "query-input": "required name=search_term_string"
+    }
+  };
+
   return (
-    <script
-      type="application/ld+json"
-      dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
-    />
+    <>
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(organizationSchema) }}
+      />
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(websiteSchema) }}
+      />
+    </>
   );
 }
